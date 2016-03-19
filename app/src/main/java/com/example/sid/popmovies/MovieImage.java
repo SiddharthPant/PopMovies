05487477b5b60848fd208f61c0a7570095dd6449
SiddharthPant <spsiddharthpant@gmail.com>
2016-03-19 19:46:44 +0530

    Create Movie Image class and image_item layout

---
+++ app/src/main/java/com/example/sid/popmovies/MovieImage.java
@@ -0,0 +1,12 @@
+package com.example.sid.popmovies;
+
+/**
+ * Created by sid on 19/3/16.
+ */
+public class MovieImage {
+    int image;
+
+    public MovieImage(int image) {
+        this.image = image;
+    }
+}
