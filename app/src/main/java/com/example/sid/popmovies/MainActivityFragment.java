783f5e9817c5b197f533a99f43482e0a4af2eea1
SiddharthPant <spsiddharthpant@gmail.com>
2016-03-20 11:53:24 +0530

    Set adapter and complete the layout

--- app/src/main/java/com/example/sid/popmovies/MainActivityFragment.java
+++ app/src/main/java/com/example/sid/popmovies/MainActivityFragment.java
@@ -5,11 +5,26 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.GridView;
+
+import java.util.Arrays;
 
 /**
  * A placeholder fragment containing a simple view.
  */
 public class MainActivityFragment extends Fragment {
+
+    private MovieImageAdapter imageAdapter;
+
+    MovieImage[] movieImages = {
+            new MovieImage(R.drawable.deadpool),
+            new MovieImage(R.drawable.hunger_games),
+            new MovieImage(R.drawable.interstellar),
+            new MovieImage(R.drawable.mad_max),
+            new MovieImage(R.drawable.zootopia),
+            new MovieImage(R.drawable.dawn_of_justice),
+            new MovieImage(R.drawable.insurgent)
+    };
 
     public MainActivityFragment() {
     }
@@ -17,6 +32,13 @@ public class MainActivityFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_main, container, false);
+        View rootView =  inflater.inflate(R.layout.fragment_main, container, false);
+
+        imageAdapter = new MovieImageAdapter(getActivity(), Arrays.asList(movieImages));
+
+        GridView gridView = (GridView) rootView.findViewById(R.id.movie_images_grid);
+        gridView.setAdapter(imageAdapter);
+
+        return rootView;
     }
 }
