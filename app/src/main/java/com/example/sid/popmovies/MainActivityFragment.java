06decf90cccda80c47f18208617ffef1c75941ab
SiddharthPant <spsiddharthpant@gmail.com>
2016-03-26 10:05:54 +0530

    Implement network code to fetch json data

--- app/src/main/java/com/example/sid/popmovies/MainActivityFragment.java
+++ app/src/main/java/com/example/sid/popmovies/MainActivityFragment.java
@@ -1,12 +1,23 @@
 package com.example.sid.popmovies;
 
+import android.net.Uri;
+import android.os.AsyncTask;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.GridView;
 
+import org.json.JSONException;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.Arrays;
 
@@ -59,5 +70,90 @@ public class MainActivityFragment extends Fragment {
         gridView.setAdapter(imageAdapter);
 
         return rootView;
+    }
+
+    private void updateMovieData() {
+        FetchMovieDetailTask movieDetailTask = new FetchMovieDetailTask();
+        movieDetailTask.execute();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        updateMovieData();
+    }
+
+    public class FetchMovieDetailTask extends AsyncTask<String, Void, String[]> {
+
+        private final String LOG_TAG = FetchMovieDetailTask.class.getSimpleName();
+
+        private String[] getMovieImageFromJson(String tmdbJsonStr) throws JSONException {
+            // TODO
+            return null;
+        }
+
+        @Override
+        protected String[] doInBackground(String... params) {
+            HttpURLConnection urlConnection = null;
+            BufferedReader reader = null;
+
+            String tmdbJsonStr = null;
+            String sortMethod = "popularity.desc";
+
+            try {
+                final String TMDB_BASE_URL = "http://api.themoviedb.org/3/discover/movie";
+                final String SORTBY_PARAM = "sort_by";
+                final String APIKEY_PARAM = "api_key";
+                Uri builtUri = Uri.parse(TMDB_BASE_URL).buildUpon()
+                        .appendQueryParameter(SORTBY_PARAM, sortMethod)
+                        .appendQueryParameter(APIKEY_PARAM, BuildConfig.THE_MOVIEDB_API_KEY).build();
+                URL url = new URL(builtUri.toString());
+                Log.v(LOG_TAG, "TMDB URL: " + url);
+
+                urlConnection = (HttpURLConnection) url.openConnection();
+                urlConnection.setRequestMethod("GET");
+                urlConnection.connect();
+
+                InputStream inputStream = urlConnection.getInputStream();
+                StringBuffer buffer = new StringBuffer();
+                if (inputStream == null) {
+                    return null;
+                }
+                reader = new BufferedReader(new InputStreamReader(inputStream));
+
+                String line;
+                while ( (line = reader.readLine()) != null) {
+                    buffer.append(line + "\n");
+                }
+
+                if (buffer.length() == 0) {
+                    return null;
+                }
+                tmdbJsonStr = buffer.toString();
+                Log.v(LOG_TAG, "TMDB JSON: " + tmdbJsonStr);
+            } catch (IOException e) {
+                Log.e(LOG_TAG, "Error ", e);
+                return null;
+            } finally {
+                if (urlConnection != null) {
+                    urlConnection.disconnect();
+                }
+                if (reader != null) {
+                    try {
+                        reader.close();
+                    } catch (final IOException e) {
+                        Log.e(LOG_TAG, "Error closing stream", e);
+                    }
+                }
+            }
+
+            try {
+                return getMovieImageFromJson(tmdbJsonStr);
+            } catch (JSONException e) {
+                Log.e(LOG_TAG, e.getMessage(), e);
+                e.printStackTrace();
+            }
+            return null;
+        }
     }
 }
